6717a1af7033cd0bb1a9f1255e6825efd2378209
Alejandra <aletugi.to@gmail.com>
2025-02-23 22:04:59 +0100

    Initial Commit

---
+++ ActividadPracticaAPI/ActividadPracticaAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActividadPracticaAPIApp.swift
+//  ActividadPracticaAPI
+//
+//  Created by Alejandra Torres on 23/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ActividadPracticaAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActividadPracticaAPIUITests/ActividadPracticaAPIUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ActividadPracticaAPIUITestsLaunchTests.swift
+//  ActividadPracticaAPIUITests
+//
+//  Created by Alejandra Torres on 23/2/25.
+//
+
+import XCTest
+
+final class ActividadPracticaAPIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
